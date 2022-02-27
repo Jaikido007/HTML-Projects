d6b0a2a635ee9d23ff3ff9e98b64ee172680631e
Jason Mullen <jason.mullen@dem-c02fk37jmd6m.home>
2022-02-27 11:12:59 +0000

    refactoring the .then method to a sync await method - both work.

--- Dad_Joke_App/js/script.js
+++ Dad_Joke_App/js/script.js
@@ -5,16 +5,32 @@ jokeBtn.addEventListener('click', generateJoke)
 
 generateJoke()
 
-function generateJoke() {
+// Async await 
+
+async function generateJoke() {
     const config = {
         headers: {
             Accept: 'application/json',
         },
     }
 
-fetch('https://icanhazdadjoke.com', config)
-    .then((resp) => resp.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke
-    })
+const res = await fetch('https://icanhazdadjoke.com', config)
+
+const data = await res.json()
+
+jokeEl.innerHTML = data.joke
 }
+
+// function generateJoke() {
+//     const config = {
+//         headers: {
+//             Accept: 'application/json',
+//         },
+//     }
+
+// fetch('https://icanhazdadjoke.com', config)
+//     .then((resp) => resp.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke
+//     })
+// }
