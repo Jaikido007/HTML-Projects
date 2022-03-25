21130417d3eca55b520812e3bf144ca769d719e8
Jason Mullen <jason.mullen@dem-c02fk37jmd6m.home>
2022-03-25 13:48:30 +0000

    Adding a loader before the quotes get pulled - won't be noticible on fast internet machines

--- Quote_Generator/js/script.js
+++ Quote_Generator/js/script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = []; // Using let not const because it's an empty array to start, which gets passed results from api changing it's value - use const if never changing
 
+// Show Loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide Loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show New Quote ------
 function newQuote() {
+    loading();
     // Pick a random quote from apiQuotes array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     // Check if Author field is blank and replace with 'Unknown'
@@ -23,14 +37,16 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
-
+    // Set Quote, Hide Laoder
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Get Quotes From API ------
 
 // asynchronous function can run anytime independently and doesn't affect the loading of a page
 async function getQuotes() { 
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl); // The const will not be populated until it has data fetched from api
