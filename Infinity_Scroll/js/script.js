5265eedcd4629745666be2a2f0ba39808ce43d11
Jason Mullen <jason.mullen@dem-c02fk37jmd6m.home>
2022-03-30 11:36:54 +0100

    Testing infinite scroll with a set random 10 images limit from unsplash using the unsplash api

--- Infinity_Scroll/js/script.js
+++ Infinity_Scroll/js/script.js
@@ -1,4 +1,7 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
+let photosArray = [];
 
 // Unsplash API
 
@@ -6,13 +9,32 @@ const count = 10;
 const apiKey = config.MY_KEY
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create elements for links and photos, Add to DOM
+function displayPhotos() {
+    // Run function for each object in phottosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to unsplash
+        const item = document.createElement('a');
+        item.setAttribute('href', photo.links.html);
+        item.setAttribute('target', '_blank');
+        // Create <img> for photo
+        const img = document.createElement('img');
+        img.setAttribute('src', photo.urls.regular);
+        img.setAttribute('alt', photo.alt_description);
+        img.setAttribute('title', photo.alt_description);
+        // Put <img> inside <a> then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
 // Get photos from Unsplash API
 
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log(data);
+        photosArray = await response.json();
+        displayPhotos()
     } catch (error) {
         // catch error here
     }
